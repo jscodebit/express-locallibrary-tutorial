d529d7ca085fbec40bd3a108edac9826de98c085
jscodebit <soujanya.janapatla@gmail.com>
2020-04-10 13:44:29 -0500

    Replaced var with let and const type

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
 //
 
-var app = express();
+let app = express();
 
 //Set up mongoose connection
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://<soujanya.janapatla2791@gmail.com>:<Creative1991@>@cluster0-hbj7r.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+let mongoose = require('mongoose');
+let mongoDB = 'mongodb+srv://<soujanya.janapatla2791@gmail.com>:<Creative1991@>@cluster0-hbj7r.azure.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-var db = mongoose.connection;
+let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
