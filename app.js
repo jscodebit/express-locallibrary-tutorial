81a8538b929dbc5f5b0bd4c004fb99e41f2ab49b
jscodebit <soujanya.janapatla@gmail.com>
2020-04-17 15:13:39 -0500

    Defined controllers and routes in app

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const dotenv = require('dotenv');
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
-//
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-let app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');
 
+const app = express();
+dotenv.config({path: '.env'});
 //Set up mongoose connection
-let mongoose = require('mongoose');
-let mongoDB = 'mongodb+srv://<soujanya.janapatla2791@gmail.com>:<Creative1991@>@cluster0-hbj7r.azure.mongodb.net/local_library?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-let db = mongoose.connection;
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const mongoDB = process.env.ATLAS_URI;
+mongoose.connect(mongoDB, {
+useNewUrlParser: true,useUnifiedTopology: true
+});
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
@@ -29,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-let express = require('express');
-let router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'WHATABYTE' });
+// GET home page.
+router.get('/', function(req, res) {
+  res.redirect('/catalog');
 });
 
 module.exports = router;
